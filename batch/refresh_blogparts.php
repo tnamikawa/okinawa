6bd517da096bf55c99687475dcf1acf3eedb5534
Takeshi NAMIKAWA <takeshi.namikawa@gmail.com>
2010-05-28 08:15:26 +0900

    hoge

--- batch/refresh_blogparts.php
+++ batch/refresh_blogparts.php
@@ -1,11 +1,8 @@
 <?php
  
-define('SF_ROOT_DIR',    realpath(dirname(__FILE__).'/..'));
-define('SF_APP',         'frontend');
-define('SF_ENVIRONMENT', 'dev');
-define('SF_DEBUG',       true);
- 
-require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
+require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
+
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
  
 // initialize database manager
 $databaseManager = new sfDatabaseManager();
